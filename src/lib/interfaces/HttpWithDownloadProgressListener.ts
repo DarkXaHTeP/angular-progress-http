8b2b1b0b97ca49c98bbf17ad243d4a0d724089b2
Volodymyr Osmak <darkxahtep@gmail.com>
2017-01-24 00:34:12 +0200

    Fixed issue with module name case

--- src/lib/interfaces/HttpWithDownloadProgressListener.ts
+++ src/lib/interfaces/HttpWithDownloadProgressListener.ts
@@ -1,6 +1,6 @@
 import { Http } from "@angular/http";
 
-import { Progress } from "./progress";
+import { Progress } from "./Progress";
 
 export interface HttpWithDownloadProgressListener extends Http {
     withUploadProgressListener(listener: (progress: Progress) => void): Http;
--- src/lib/interfaces/HttpWithUploadProgressListener.ts
+++ src/lib/interfaces/HttpWithUploadProgressListener.ts
@@ -1,6 +1,6 @@
 import { Http } from "@angular/http";
 
-import { Progress } from "./progress";
+import { Progress } from "./Progress";
 
 export interface HttpWithUploadProgressListener extends Http {
     withDownloadProgressListener(listener: (progress: Progress) => void): Http;
