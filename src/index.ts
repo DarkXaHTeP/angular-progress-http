694398acc647f738a61b32a3b8df837d073f68f4
Volodymyr Osmak <darkxahtep@gmail.com>
2017-03-23 01:41:37 +0200

    Added required exports for module

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { ProgressHttpModule } from "./lib/progress-http.module";
 export { ProgressHttp } from "./lib/progress-http.service";
-export { HttpWithDownloadProgressListener, HttpWithUploadProgressListener } from "./lib/interfaces";
+export { HttpWithDownloadProgressListener, HttpWithUploadProgressListener, Progress, HttpFactory } from "./lib/interfaces";
+export { HTTP_FACTORY } from "./lib/http-factory.token";
