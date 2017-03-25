c03ae3f8c147db9aa1823d5cae4edf703706324c
Volodymyr Osmak <darkxahtep@gmail.com>
2017-03-26 01:40:24 +0200

    Added back example app and removed dist folder

--- examples/upload-download/src/app/app.component.ts
+++ examples/upload-download/src/app/app.component.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import { ProgressHttp } from 'angular-progress-http';
+
+interface FileDescriptor {
+  name: string;
+  file: File;
+  uploaded: boolean;
+  percentage?: number;
+}
 
 @Component({
   selector: 'aph-root',
@@ -6,5 +14,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'aph works!';
+  public files: FileDescriptor[] = [];
+
+  constructor(private progressHttp: ProgressHttp) {
+  }
+
+  public onFilesSelected(fileList: FileList) {
+    this.files = Array.from(fileList).map((f: File): FileDescriptor => ({
+      name: f.name,
+      file: f,
+      uploaded: false,
+      percentage: null
+    }))
+  }
+
+  public upload() {
+    this.files.forEach(f => {
+      let form = new FormData();
+      form.append("file", f.file);
+
+      this.progressHttp
+        .withUploadProgressListener(progress => { f.percentage = progress.percentage; })
+        .post("/fileUpload", form)
+        .subscribe((r) => {
+          f.uploaded = true;
+        })
+    });
+  }
 }
