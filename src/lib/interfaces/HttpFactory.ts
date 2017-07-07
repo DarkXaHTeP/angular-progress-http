49c25da9d0491595bc2c5b2418a62f7c1c892ea9
Volodymyr Osmak <darkxahtep@gmail.com>
2017-07-07 14:52:48 +0300

    Fixed interface generic method error after TypeScript update

--- src/lib/interfaces/HttpFactory.ts
+++ src/lib/interfaces/HttpFactory.ts
@@ -1,5 +1,5 @@
 import { Http, ConnectionBackend, RequestOptions } from "@angular/http";
 
 export interface HttpFactory {
-    create<T extends Http>(backend: ConnectionBackend, requestOptions: RequestOptions): T;
+    create<T extends Http>(backend: ConnectionBackend, requestOptions: RequestOptions): T | Http;
 }
