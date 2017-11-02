172beed645d7d550fd0ce272db29dd5b98232da7
Volodymyr Osmak <darkxahtep@gmail.com>
2017-11-03 00:20:01 +0200

    Added type to InjectionToken

--- src/lib/http-factory.token.ts
+++ src/lib/http-factory.token.ts
@@ -1,3 +1,4 @@
 import { InjectionToken } from "@angular/core";
+import { HttpFactory } from "./interfaces/HttpFactory";
 
-export const HTTP_FACTORY = new InjectionToken("Http implementation factory");
+export const HTTP_FACTORY = new InjectionToken<HttpFactory>("Http implementation factory");
