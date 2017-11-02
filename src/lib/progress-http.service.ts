82b53012a98f5343f41b3dc20748ce0c23ed1de5
Volodymyr Osmak <darkxahtep@gmail.com>
2017-11-03 00:20:28 +0200

    Removed @Inject decorator from service dependency

--- src/lib/progress-http.service.ts
+++ src/lib/progress-http.service.ts
@@ -1,11 +1,10 @@
-import { Injectable, Inject } from "@angular/core";
+import { Injectable } from "@angular/core";
 import { Http, RequestOptionsArgs, RequestOptions, Request, Response, ConnectionBackend } from "@angular/http";
 import { Observable } from "rxjs/Observable";
 
 import { HttpWithDownloadProgressListener, HttpWithUploadProgressListener, Progress } from "./interfaces";
 import { XHRBackendFactory } from "./XHRBackendFactory";
 
-import { HTTP_FACTORY } from "./http-factory.token";
 import { HttpFactory } from "./interfaces";
 
 @Injectable()
@@ -17,7 +16,7 @@ export class ProgressHttp extends Http implements HttpWithUploadProgressListener
     public constructor(
         private xhrBackendFactory: XHRBackendFactory,
         private requestOptions: RequestOptions,
-        @Inject(HTTP_FACTORY) private httpFactory: HttpFactory,
+        private httpFactory: HttpFactory,
         backend: ConnectionBackend
     ) {
         super(null, requestOptions);
