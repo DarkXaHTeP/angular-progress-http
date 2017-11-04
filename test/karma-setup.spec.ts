2f3e1edf3d77652a471b26ea6edb7ab2f4fe0a29
Volodymyr Osmak <darkxahtep@gmail.com>
2017-11-04 17:10:51 +0200

    Set up testing using Angular CLI and Karma

--- test/karma-setup.spec.ts
+++ test/karma-setup.spec.ts
@@ -1,3 +1,14 @@
+import { Progress } from "angular-progress-http";
+
 describe('Jasmine', () => {
     it('runs true is true', () => expect(true).toBeTruthy());
+    it("can access built lib", () => {
+        const progress: Progress = {
+            event: <any>{},
+            lengthComputable: true,
+            percentage: 34,
+            loaded: 232,
+            total: 350
+        };
+    })
 });
