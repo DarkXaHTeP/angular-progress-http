d9c1da47c17d9d463654bdbc0701e1f850024f71
Volodymyr Osmak <darkxahtep@gmail.com>
2017-11-04 17:25:06 +0200

    Added fix for Chrome on Travis CI

--- karma.conf.js
+++ karma.conf.js
@@ -2,33 +2,45 @@
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
 module.exports = function (config) {
-  config.set({
-    basePath: '',
-    frameworks: ['jasmine', '@angular/cli'],
-    plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
-      require('@angular/cli/plugins/karma')
-    ],
-    client:{
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
-    },
-    coverageIstanbulReporter: {
-      reports: [ 'html', 'lcovonly' ],
-      fixWebpackSourcePaths: true
-    },
-    angularCli: {
-      environment: 'dev',
-      codeCoverage: true
-    },
-    reporters: ['progress', 'kjhtml'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false
-  });
+  const configuration = {
+      basePath: '',
+      frameworks: ['jasmine', '@angular/cli'],
+      plugins: [
+          require('karma-jasmine'),
+          require('karma-chrome-launcher'),
+          require('karma-jasmine-html-reporter'),
+          require('karma-coverage-istanbul-reporter'),
+          require('@angular/cli/plugins/karma')
+      ],
+      client:{
+          clearContext: false // leave Jasmine Spec Runner output visible in browser
+      },
+      customLaunchers: {
+          Chrome_travis_ci: {
+              base: 'Chrome',
+              flags: ['--no-sandbox']
+          }
+      },
+      coverageIstanbulReporter: {
+          reports: [ 'html', 'lcovonly' ],
+          fixWebpackSourcePaths: true
+      },
+      angularCli: {
+          environment: 'dev',
+          codeCoverage: true
+      },
+      reporters: ['progress', 'kjhtml'],
+      port: 9876,
+      colors: true,
+      logLevel: config.LOG_INFO,
+      autoWatch: true,
+      browsers: ['Chrome'],
+      singleRun: false
+  };
+
+  if (process.env.TRAVIS) {
+      configuration.browsers = ['Chrome_travis_ci'];
+  }
+
+  config.set(configuration);
 };
