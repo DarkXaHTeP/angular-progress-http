fe884e6f12870c2822e70275f12b7444bdcefde9
Volodymyr Osmak <darkxahtep@gmail.com>
2017-11-06 02:43:35 +0200

    Added custom http tests

---
+++ test/specs/progress-http.service.custom-http.spec.ts
@@ -0,0 +1,108 @@
+import {
+    async,
+    getTestBed,
+    TestBed
+} from '@angular/core/testing';
+
+import "rxjs/add/operator/map";
+
+import xhrMock from 'xhr-mock';
+
+import {HttpModule} from "@angular/http";
+
+import {ProgressHttpModule, ProgressHttp, HTTP_FACTORY} from "angular-progress-http";
+
+import {CustomHttpFactory, CustomHttp} from "../mocks";
+
+describe("ProgressHttp with custom Http Factory", () => {
+    let testBed;
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [
+                HttpModule,
+                ProgressHttpModule
+            ],
+            providers: [
+                {provide: HTTP_FACTORY, useClass: CustomHttpFactory}
+            ]
+        });
+
+        testBed = getTestBed();
+    }));
+
+    describe("calls factory create method when", () => {
+        it("gets constructed", () => {
+            spyOn(CustomHttpFactory.prototype, "create").and.callThrough();
+
+            testBed.get(ProgressHttp);
+
+            expect(CustomHttpFactory.prototype.create).toHaveBeenCalledTimes(1);
+
+        });
+
+        describe("listener", () => {
+            let progressHttp;
+
+            beforeEach(() => {
+                progressHttp = testBed.get(ProgressHttp);
+            });
+
+            it("is added to upload", () => {
+                spyOn(CustomHttpFactory.prototype, "create").and.callThrough();
+
+                progressHttp
+                    .withUploadProgressListener(() => {
+                    });
+
+                expect(CustomHttpFactory.prototype.create).toHaveBeenCalledTimes(1);
+            });
+
+            it("is added to download", () => {
+                spyOn(CustomHttpFactory.prototype, "create").and.callThrough();
+
+                progressHttp
+                    .withDownloadProgressListener(() => {
+                    });
+
+                expect(CustomHttpFactory.prototype.create).toHaveBeenCalledTimes(1);
+            });
+
+            it("is added to both", () => {
+                spyOn(CustomHttpFactory.prototype, "create").and.callThrough();
+
+                progressHttp
+                    .withUploadProgressListener(() => {
+                    })
+                    .withDownloadProgressListener(() => {
+                    });
+
+                expect(CustomHttpFactory.prototype.create).toHaveBeenCalledTimes(2);
+            });
+        });
+
+    })
+
+    describe("uses custom http methods when", () => {
+        it("executes http calls", (done) => {
+            xhrMock.setup();
+
+            xhrMock.get("/data", (req, res) => {
+                return res;
+            });
+
+            spyOn(CustomHttp.prototype, "request").and.callThrough();
+
+            const progressHttp = testBed.get(ProgressHttp);
+
+            progressHttp
+                .get("/data")
+                .subscribe(() => {
+                    expect(CustomHttp.prototype.request).toHaveBeenCalledTimes(1);
+                    done();
+                });
+
+            xhrMock.teardown();
+        });
+    });
+});
